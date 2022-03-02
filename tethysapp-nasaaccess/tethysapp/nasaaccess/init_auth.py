0b732a3ade996c0f31296f225c1ee1bb8f44b009
romer8 <gio.busrom@gmail.com>
2022-03-02 12:41:25 -0700

    automatically auth added

--- tethysapp-nasaaccess/tethysapp/nasaaccess/init_auth.py
+++ tethysapp-nasaaccess/tethysapp/nasaaccess/init_auth.py
@@ -1,4 +1,6 @@
 import os
+from .config import *
+
 # from .helpers import *
 # logger.info("Creating authentication files")
 
@@ -37,3 +39,24 @@ def make_auth_files():
     except Exception as e:
         # logger.info(e)
         print(e)
+
+
+def write_new_engine():
+    machine = "urs.earthdata.nasa.gov"
+    user = nasa_user
+    pswd = nasa_password
+    message = {}
+    file = open(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                             'workspaces', 'app_workspace', '.netrc'), 'a+')
+    lines = file.readlines()
+    if len(lines) == 0:
+        file.write('machine ' + machine + ' login ' + user + ' password ' + pswd + '\n')
+    elif lines[-1] == '\n':
+        file.write('machine ' + machine + ' login ' + user + ' password ' + pswd + '\n')
+    else:
+        file.write('machine ' + machine + ' login ' + user + ' password ' + pswd + '\n')
+
+    file.close()
+
+    message['message'] = 'Authentication Successfully Added'
+    return message
--- tethysapp-nasaaccess/tethysapp/nasaaccess/init_stores.py
+++ tethysapp-nasaaccess/tethysapp/nasaaccess/init_stores.py
@@ -1,7 +1,7 @@
 from .model import Base , DEMfiles, Shapefiles, accessCode
 from .app import nasaaccess as app
 from sqlalchemy.orm import sessionmaker
-from .init_auth import make_auth_files
+from .init_auth import *
 
 
 # Initialize an empty database, if the database has not been created already.
@@ -12,6 +12,7 @@ def init_db(engine, first_time):
     print("Initializing Persistant Storage")
     Base.metadata.create_all(engine)
     make_auth_files()
+    write_new_engine()
     if first_time:
     # # Make session
         SessionMaker = sessionmaker(bind=engine)
