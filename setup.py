5b6ceeaa448a897407e359d09e8ca2d887564326
romer8 <gio.busrom@gmail.com>
2022-03-23 09:28:26 -0600

    changes to be a tethys 3 app

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
-import os
-import sys
 from setuptools import setup, find_packages
-# from tethys_apps.app_installation import custom_develop_command, custom_install_command
 from tethys_apps.app_installation import find_resource_files
 
 ### Apps Definition ###
 app_package = 'nasaaccess'
 release_package = 'tethysapp-' + app_package
-# app_class = 'nasaaccess.app:Nasaaccess'
-# app_package_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tethysapp', app_package)
 
 ### Python Dependencies ###
 dependencies = []
@@ -22,21 +17,15 @@ resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces'
 setup(
     name=release_package,
     version='1.0.0',
-    # tags='&quot;Hydrology&quot;, &quot;GLDAS&quot;, &quot;GPM&quot;, &quot;SWAT&quot;',
     description='Web interface for downloading precipitation and air temperature data from NASA&#39;s EarthData website',
     long_description='',
-    keywords='',
-    author='Spencer McDonald, Ibrahim Mohammed',
+    keywords='"Hydrology", "GLDAS&quot", "GPM", "SWAT"',
+    author='Spencer McDonald, Ibrahim Mohammed, Giovanni Romero',
     author_email='ibrahim.mohammed@nasa.gov',
     url='',
     license='NASA OPEN SOURCE AGREEMENT VERSION 1.3',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    # namespace_packages=['tethysapp', 'tethysapp.' + app_package],
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=dependencies
-    # cmdclass={
-    #     'install': custom_install_command(app_package, app_package_dir, dependencies),
-    #     'develop': custom_develop_command(app_package, app_package_dir, dependencies)
-    # }
 )
