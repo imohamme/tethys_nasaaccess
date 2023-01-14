2583227c524407cd172322b110d54b4d273ca979
romer8 <gio.busrom@gmail.com>
2023-01-14 16:53:27 -0700

    docker compliant step 1

--- tethysapp/nasaaccess/app.py
+++ tethysapp/nasaaccess/app.py
@@ -19,57 +19,57 @@ class nasaaccess(TethysAppBase):
     feedback_emails = []
     controller_modules = [ "controllers", "ajax_controllers"]
 
-    ## custom settings ##
-    # def custom_settings(self):
-    #     """
-    #     Example custom_settings method.
-    #     """
-    #     custom_settings = (
-    #         CustomSetting(
-    #             name="data_path",
-    #             type=CustomSetting.TYPE_STRING,
-    #             description="Data Directory for Downloads",
-    #             required=False,
-    #         ),
-    #         CustomSetting(
-    #             name="nasaaccess_R",
-    #             type=CustomSetting.TYPE_STRING,
-    #             description="R interpreter",
-    #             required=False,
-    #         ),
-    #         CustomSetting(
-    #             name="nasaaccess_script",
-    #             type=CustomSetting.TYPE_STRING,
-    #             description="Path to the nasaaccess R script file",
-    #             required=False,
-    #         ),
-    #         CustomSetting(
-    #             name="geoserver_workspace",
-    #             type=CustomSetting.TYPE_STRING,
-    #             description="Geoserver Workspace",
-    #             required=False,
-    #         ),
-    #         CustomSetting(
-    #             name="geoserver_URI",
-    #             type=CustomSetting.TYPE_STRING,
-    #             description="Geoserver URI",
-    #             required=False,
-    #         ),
-    #         CustomSetting(
-    #             name="geoserver_user",
-    #             type=CustomSetting.TYPE_STRING,
-    #             description="Geoserver User",
-    #             required=False,
-    #         ),
-    #         CustomSetting(
-    #             name="geoserver_password",
-    #             type=CustomSetting.TYPE_STRING,
-    #             description="Geoserver Password",
-    #             required=False,
-    #         ),
-    #     )
+    # custom settings ##
+    def custom_settings(self):
+        """
+        Example custom_settings method.
+        """
+        custom_settings = (
+            CustomSetting(
+                name="data_path",
+                type=CustomSetting.TYPE_STRING,
+                description="Data Directory for Downloads",
+                required=False,
+            ),
+            CustomSetting(
+                name="nasaaccess_R",
+                type=CustomSetting.TYPE_STRING,
+                description="R interpreter",
+                required=False,
+            ),
+            CustomSetting(
+                name="nasaaccess_script",
+                type=CustomSetting.TYPE_STRING,
+                description="Path to the nasaaccess R script file",
+                required=False,
+            ),
+            CustomSetting(
+                name="geoserver_workspace",
+                type=CustomSetting.TYPE_STRING,
+                description="Geoserver Workspace",
+                required=False,
+            ),
+            CustomSetting(
+                name="geoserver_URI",
+                type=CustomSetting.TYPE_STRING,
+                description="Geoserver URI",
+                required=False,
+            ),
+            CustomSetting(
+                name="geoserver_user",
+                type=CustomSetting.TYPE_STRING,
+                description="Geoserver User",
+                required=False,
+            ),
+            CustomSetting(
+                name="geoserver_password",
+                type=CustomSetting.TYPE_STRING,
+                description="Geoserver Password",
+                required=False,
+            ),
+        )
 
-    #     return custom_settings
+        return custom_settings
 
     def spatial_dataset_service_settings(self):
         """
